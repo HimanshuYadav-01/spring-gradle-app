98ced58a3aba3615073dd9813154524dd4e88365
HimanshuYadav-01 <152787856+HimanshuYadav-01@users.noreply.github.com>
2025-04-24 19:26:44 +0530

    basic change of message

--- src/main/java/com/example/HelloController.java
+++ src/main/java/com/example/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/")
     public String hello() {
-        return "Hello from Azure VM!";
+        return "Hello we have succefully implemented cicd on simple java application using gradel for building the app";
     }
 }
